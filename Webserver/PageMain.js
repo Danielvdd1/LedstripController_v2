2a7a6f507e2cecfde0c38064cbb74bf034ffe119
Daniël van der Drift <danielvdd1@hotmail.com>
2021-02-02 14:07:25 +0100

    Small mistake fix

--- Webserver/PageMain.js
+++ Webserver/PageMain.js
@@ -172,7 +172,7 @@ function SendW(id, valW) {
 }
 
 function SendOnOff() {
-    console.log('Send: /onoff');
+    console.log('Send: /onoff?s=1');
     var xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = function () {
         if (this.readyState == 4 && this.status == 200) {
